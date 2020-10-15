2993b756731abcc5357cb366c21967af08189ba9
feeha <fehagr8@gmail.com>
2020-10-15 22:36:59 +0500

    Updates

---
+++ precache-manifest.ac18f92079dcbf08c58bed11fa1362ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4297d4dfb3906528795b9007ef1403cc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "609af109d5cf5438ca5a",
+    "url": "/monsters-rolodex/static/css/main.4419fe91.chunk.css"
+  },
+  {
+    "revision": "0f0c21fb3ecefeeb9731",
+    "url": "/monsters-rolodex/static/js/2.6bf81cde.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.6bf81cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609af109d5cf5438ca5a",
+    "url": "/monsters-rolodex/static/js/main.a90c8a1f.chunk.js"
+  },
+  {
+    "revision": "3ba8d793a0518f02f613",
+    "url": "/monsters-rolodex/static/js/runtime-main.1a64cfea.js"
+  }
+]);
